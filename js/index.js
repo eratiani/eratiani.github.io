5a80ccd89f4bb3202c0589ff056d28322c96c309
erekle ratiani <62502052+eratiani@users.noreply.github.com>
2022-04-30 13:48:38 +0400

    refactor:
    added js

---
+++ js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 3,
+        loop: true,
+        nav: true,
+        navText:["<img src='./images/arrowleft.svg'>","<img src='./images/arrowright.svg'>"],
+    });
+  });
