0117cd2412ca2228a3e4f23ae94ba78570a4a50b
erekle ratiani <62502052+eratiani@users.noreply.github.com>
2022-06-11 22:47:05 +0400

    docs:
    fixed jquery

--- js/index.js
+++ js/index.js
@@ -710,16 +710,32 @@ const pet = [
     //     });
     // });
     ////////////////////burger
-    $('.burger').click(function(){
-        $(this).toggleClass('open');
-        $(".navBurger").toggleClass("closed")
+//     $('.burger').click(function(){
+//         $(this).toggleClass('open');
+//         $(".navBurger").toggleClass("closed")
+        
+//       });
+//     $(".navBurger").click(()=>{
+//         if ($('.navBurger').classList = "open" ) {
+    
+//             $(".burger").toggleClass('open');
+//             $(".navBurger").toggleClass("closed")
+//         }
+        
+       
+//     });
+   let burger = document.querySelector(".burger")
+    let navBurger = document.querySelector(".navBurger")
+    burger.addEventListener("click", function(){
+        burger.classList.toggle('open');
+        navBurger.classList.toggle("closed")
         
       });
-    $(".navBurger").click(()=>{
-        if ($('.navBurger').classList = "open" ) {
+    navBurger.addEventListener("click", function(){
+        if (navBurger.classList = "open" ) {
     
-            $(".burger").toggleClass('open');
-            $(".navBurger").toggleClass("closed")
+            burger.classList.toggle('open');
+            navBurger.classList.toggle("closed")
         }
         
        
